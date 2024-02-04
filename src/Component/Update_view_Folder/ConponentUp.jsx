3e6f5d3e4b851af62f9bd1a6d13fe092c03ab845
driss <jabri.dri.fst@uhp.ac.ma>
2024-02-04 16:32:26 +0100

    Ajout Hotel

--- src/Component/Update_view_Folder/ConponentUp.jsx
+++ src/Component/Update_view_Folder/ConponentUp.jsx
@@ -1,39 +1,39 @@
-import React, { useState } from 'react';
-import './ConponentUp.css';
-export default function ToggleContent({}) {
-    const [isVisible, setIsVisible] = useState(false);
+// import React, { useState } from 'react';
+// import './ConponentUp.css';
+// export default function ToggleContent({}) {
+//     const [isVisible, setIsVisible] = useState(false);
     
 
-    const toggleVisibility = () => {
-        setIsVisible(!isVisible); 
-        if(!isVisible){
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth' // Pour un défilement fluide
-        });
-        document.body.classList.add('disable-scroll');}
-        else
-        document.body.classList.remove('disable-scroll');
+//     const toggleVisibility = () => {
+//         setIsVisible(!isVisible); 
+//         if(!isVisible){
+//         window.scrollTo({
+//             top: 0,
+//             behavior: 'smooth' // Pour un défilement fluide
+//         });
+//         document.body.classList.add('disable-scroll');}
+//         else
+//         document.body.classList.remove('disable-scroll');
 
-    };
+//     };
 
-    return (
-        <div className='cg'>
-            <button onClick={toggleVisibility}>Toggle Content</button>
-            {isVisible && (
-                <div className='cm'>
+//     return (
+//         <div className='cg'>
+//             <button onClick={toggleVisibility}>Toggle Content</button>
+//             {isVisible && (
+//                 <div className='cm'>
                    
-                        <div className='cmc'>
+//                         <div className='cmc'>
                             
-                                <button onClick={toggleVisibility}>Toggle Content</button>
-                                {/* Contenu à afficher lorsqu'il est visible */}
-                                <input type="email" name="" placeholder='email' id="" />
+//                                 <button onClick={toggleVisibility}>Toggle Content</button>
+//                                 {/* Contenu à afficher lorsqu'il est visible */}
+//                                 <input type="email" name="" placeholder='email' id="" />
                                 
                                 
-                        </div>
+//                         </div>
                     
-                </div>
-            )}
-        </div>
-    );
-}
+//                 </div>
+//             )}
+//         </div>
+//     );
+// }
